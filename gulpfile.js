9b3e1e87af970345688b6b758ac4de84cabd8ec4
ann-1986 <ann-1986@yandex.ru>
2022-03-12 17:54:04 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,231 @@
+'use strict';
+
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	prefixer = require('gulp-autoprefixer'),
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	rigger = require('gulp-rigger'),
+	cleanCss = require('gulp-clean-css'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant'),
+	svgSprite = require('gulp-svg-sprite'),
+	svgmin = require('gulp-svgmin'),
+	replace = require('gulp-replace'),
+	browserSync = require("browser-sync").create(),
+	del = require("del"),
+	cache = require("gulp-cache"),
+	cheerio = require('gulp-cheerio'),
+	imageminJpegRecompress = require('imagemin-jpeg-recompress'),
+	include = require('gulp-html-tag-include'),
+	merge = require('merge-stream'),
+	spritesmith = require('gulp.spritesmith');
+
+
+var path = {
+	//папка куда складываются готовые файлы
+	build: {
+		html: 'build/',
+		js: 'build/js/',
+		css: 'build/css/',
+		img: 'build/img/',
+		fonts: 'build/fonts/',
+		spritePng: 'build/img/'
+	},
+	//папка откуда брать файлы
+	src: {
+		html: 'src/**/!(*.inc).html',
+		js: [
+			'src/vendor/jquery-3.2.1.min.js', 
+			'src/vendor/slick/slick.min.js',
+			'src/vendor/owlcarousel/owl.carousel.min.js',
+			'src/vendor/jquery.inputmask.bundle.js',
+			'src/vendor/fancybox/jquery.fancybox.min.js',
+			'src/vendor/wow.min.js',
+			'src/js/**/*.js', 
+			'src/js/main.js'
+		],
+		style: 'src/sass/main.scss',
+		css: 'src/css/*.css',
+		img: 'src/img/**/*.*',
+		fonts: 'src/fonts/**/*.*',
+		spritePng: 'src/sprites/*.*'
+	},
+	//указываем после измененя каких файлов нужно действовать
+	watch: {
+		html: 'src/**/*.html',
+		js: 'src/js/**/*.js',
+		style: 'src/sass/**/*.scss',
+		css: 'src/css/**/*.css',
+		img: 'src/img/**/*.*',
+		svg: 'src/img/sprite/**/*.svg',
+		fonts: 'src/fonts/**/*.*',
+		spritePng: 'src/sprites/*.*',
+	},
+	clean: './build'
+};
+
+var config = {
+	server: {
+		baseDir: "./build" //из какой папки показывать
+	},
+	tunnel: false,
+	host: 'localhost',
+	port: 8080,
+	open: true,
+	notify: false,
+	logPrefix: "gl"
+};
+
+function fonts() {
+	return gulp.src(path.src.fonts)
+		.pipe(gulp.dest(path.build.fonts));
+}
+
+function images() {
+	return gulp.src(path.src.img)
+		.pipe(cache(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.jpegtran({ progressive: true }),
+			imageminJpegRecompress({
+				loops: 5,
+				min: 65,
+				max: 85,
+				quality: 'high'
+			}),
+			imagemin.svgo({
+				plugins: [
+					{ cleanupIDs: false },
+					{ removeUselessDefs: false },
+					{ removeViewBox: true },
+				]
+			}),
+			imagemin.optipng({ optimizationLevel: 3 }),
+			pngquant({ quality: '65-70', speed: 5 })
+		], {
+			verbose: true
+		})))
+		.pipe(gulp.dest(path.build.img))
+		.pipe(browserSync.reload({ stream: true }));
+}
+function sprite() {
+	return gulp.src('src/img/sprite/**/*.svg')
+		.pipe(svgmin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		.pipe(cheerio({
+			run: function ($) {
+				// $('[fill]').removeAttr('fill');
+				// $('[stroke]').removeAttr('stroke');
+				// $('[style]').removeAttr('style');
+			},
+			parserOptions: { xmlMode: true }
+		}))
+		.pipe(replace('&gt;', '>'))
+		.pipe(svgSprite({
+			shape: {
+				transform: []
+			},
+			mode: {
+				symbol: {
+					sprite: "../sprite.svg",
+				}
+			}
+		}))
+
+		.pipe(gulp.dest('src/img'))
+}
+
+function spritePng() {
+	var spriteData = gulp.src(path.src.spritePng).pipe(spritesmith({
+		imgName: 'sprite.png',
+		imgPath: '/img/sprite.png',
+		cssName: 'sprite.scss'
+	}));
+	var imgStream = spriteData.img
+		.pipe(gulp.dest(path.build.spritePng));
+
+	var cssStream = spriteData.css
+		.pipe(gulp.dest('src/sass/'));
+	return merge(imgStream, cssStream);
+}
+
+function html() {
+	return gulp.src(path.src.html)
+		.pipe(rigger())
+		.pipe(include())
+		.pipe(gulp.dest(path.build.html))
+		.pipe(browserSync.reload({ stream: true }));
+}
+
+function styles() {
+	return gulp.src(path.src.style)
+		.pipe(sourcemaps.init())
+		
+		.pipe(sass())
+		
+		.pipe(prefixer({
+			overrideBrowserslist: ['> 0.5%', 'last 2 version'],
+			cascade: false
+		}))
+		.pipe(cleanCss({
+			level: 2
+		}))
+		.pipe(concat("app.css"))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.build.css))
+		.pipe(browserSync.reload({ stream: true }));
+}
+function css() {
+	return gulp.src(path.src.css)
+		.pipe(gulp.dest(path.build.css))
+		.pipe(browserSync.reload({ stream: true }));
+}
+
+function scripts() {
+	return gulp.src(path.src.js)
+		.pipe(rigger())
+		.pipe(sourcemaps.init())
+		.pipe(concat('all.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(path.build.js))
+		.pipe(browserSync.reload({ stream: true }));
+}
+
+function watch() {
+	browserSync.init(config)
+	gulp.watch(path.watch.spritePng, spritePng);
+	gulp.watch(path.watch.style, styles);
+	gulp.watch(path.watch.css, css);
+	gulp.watch(path.watch.js, scripts);
+	gulp.watch(path.watch.html, html);
+	gulp.watch(path.watch.fonts, fonts);
+	gulp.watch(path.watch.img, images);
+	gulp.watch(path.watch.svg, sprite);
+
+
+}
+
+function clean() {
+	return del(['build/*']);
+}
+
+gulp.task('fonts', fonts);
+gulp.task('sprite', sprite);
+gulp.task('images', images);
+gulp.task('html', html);
+gulp.task('styles', styles);
+gulp.task('css', css);
+gulp.task('scripts', scripts);
+gulp.task('watch', watch);
+gulp.task('spritePng', spritePng);
+
+gulp.task('build', gulp.series(clean,
+	gulp.parallel(spritePng, styles, css, scripts, html, sprite, images, fonts
+	)));
+
+gulp.task('dev', gulp.series('spritePng', 'build', 'watch'));
+
---
+++ src/js/main.js
@@ -0,0 +1,402 @@
+
+$(function() {
+    //маска телефонов
+    $('[data-mask]').each(function() {
+        input = $(this);
+        mask = input.attr('data-mask');
+        input.inputmask({"mask": mask});
+    });
+
+    //сворачивание мобильного меню
+    $('.toggle-menu-js').on('click', function (e) {
+        e.preventDefault();
+        $('body').removeClass('show-phones-menu')
+        if ($('body').hasClass('show-slide-menu')) {
+            hideSlideMenu();
+        } else {
+            showSlideMenu();
+        }
+    });
+
+    $('body').on('click', '[data-menu-show]', function() {
+        $('[data-menu-show]').removeClass("active");
+        $(this).addClass("active");
+
+        var menuId = $(this).attr('data-menu-show');
+        $('[data-menu-panel]').removeClass("active");
+        $('[data-menu-panel="'+menuId+'"]').addClass("active");
+    });
+
+    //плавный переход по контенту
+    $('body').on('click', '[data-goto]', function(e) {
+        e.preventDefault();
+        $('.mobile-menu').slideUp();
+        var hx = 0;
+        var selector = $(this).attr('data-goto');
+        $('html, body').animate({ scrollTop: $(selector).offset().top + hx}, 1200);
+    });
+
+    //обрабатываем событие клика по табу
+    $('[data-tab]').click(function(e){
+        e.preventDefault();
+        if ($(this).hasClass('active')) return false;
+
+        var parent = $(this).parents('.xtab-container');
+        var xtab = parent.find('.xtab');
+
+        xtab.stop(true, true);
+        parent.find('[data-tab]').removeClass('active');
+
+        var data_tab = $(this).attr('data-tab');
+        
+        $(this).addClass('active');
+        //$('[data-tab="'+data_tab+'"]').addClass('active');
+        xtab.animate({ "opacity": 0.2 }, 150, function() {
+            xtab.removeClass('active');
+            xtab.animate({"opacity": 1});
+            $(data_tab).addClass('active');
+        });
+        return false;
+    });
+
+    // слайдер на главной
+    initBestSlider();
+
+    // слайдер Инстаграм на главной
+    initInstagramSlider();
+
+    // слайдер Наши клиенты
+    initClientSlider();
+
+    // слайдер Новости
+    initNewsSlider();
+
+    // слайдер Статьи
+    initArticlesSlider();
+
+    // слайдер категорий в мобильном меню
+    initMobileCategoryMenu();
+    
+    initXtab();
+    /*
+    toogler({
+        "parent": ".footer-block",
+        "hiddenContainer": ".footer-block__body",
+        "link": ".footer-block__arrow",
+        "speed": 100,
+    });
+    */
+
+    //плавный переход по контенту
+    $('body').on('click', '[data-goto]', function(e) {
+        e.preventDefault();
+        $('.mobile-menu').slideUp();
+        var hx = 0;
+        var selector = $(this).attr('data-goto');
+        $('html, body').animate({ scrollTop: $(selector).offset().top + hx}, 1200);
+    });
+
+    $('.sticky-menu-item').on('click', function(e) {
+        e.preventDefault();
+        
+        $('.sticky-menu-item').removeClass('active');
+        $(this).addClass('active');
+        $('.sticky-menu').addClass('open');
+
+        var submenuId = $(this).attr('data-submenu');
+ 
+        $('.sticky-submenu').removeClass('active');
+        setTimeout(function() {
+            $('.sticky-submenu[data-submenu="'+submenuId+'"]').addClass('active');
+        }, 500);
+
+        if ($(this).hasClass('sticky-menu-item--search')) {
+            $(this).find('input').focus();
+        }
+        
+        
+    });
+
+    $('body').on('click', function(e) {
+        if ($(e.target).parents('.sticky-menu').length == 0) {
+            closeStickyMenu();
+        }
+    });
+
+    $('.sticky-submenu-cross').on('click', function(e) {
+        e.preventDefault();
+        closeStickyMenu();
+    })
+
+    
+    $('body').on('click', '.footer-block__arrow', function(e) {
+        e.preventDefault();
+        console.log("click");
+        $(this).parents('.footer-block').toggleClass('opened');
+    });
+
+    $('[data-fancybox]').on('click', function() {
+        hideSlideMenu();
+    });
+
+    new WOW().init();
+
+    $(document).scroll(function(){
+        setFixedHeader();
+    });
+});
+
+$("body").on("click", "[data-hide-menu]", function() {
+    hideSlideMenu();
+});
+
+var hideSlideMenu = function() {
+    $('body').removeClass('show-slide-menu');
+};
+
+var showSlideMenu = function() {
+    $('[data-menu-panel]').removeClass("active");
+    $('[data-menu-panel="catalog"]').addClass("active");
+    $('body').addClass('show-slide-menu');
+};
+
+
+var closeStickyMenu = function() {
+    $('.sticky-submenu').removeClass('active');
+
+    setTimeout(function() {
+        $('.sticky-menu-item').removeClass('active');
+        $('.sticky-menu').removeClass('open');
+    }, 300);
+}
+
+var initMobileCategoryMenu = function() {
+    $('.mobile-category-menu-js').owlCarousel({
+        margin: 20,
+        loop: false,
+        autoWidth: true,
+        items: 3,
+        dots: false,
+    })
+}
+
+var initBestSlider = function() {
+    $('.best-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+    });
+};
+
+var initClientSlider = function() {
+    $('.client-slider').slick({
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                }
+            },
+            {
+                breakpoint: 2000,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            }
+        ]
+    });
+};
+
+var initInstagramSlider = function() {
+    $('.instagram-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 420,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }
+        ]
+    });
+};
+
+var initNewsSlider = function() {
+    $('.news-slider-js').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+};
+
+var initArticlesSlider = function() {
+    $('.articles-slider-js').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+};
+
+var initXtab = function() {
+    setTimeout(function() {
+        $('.xtab-container').each(function() {
+            $(this).addClass('xtab-initialized');
+        });
+    }, 100);
+};
+
+var toogler = function(ops) {
+    var defaultOptions = {
+        "parent": "body",
+        "speed": 300,
+        "openClass": "opened",
+        "stepCorrection": true,
+    };
+
+    // var options = Object.assign({}, defaultOptions, ops);
+    var options = $.extend({}, defaultOptions, ops);
+
+    $("body").on("click", options.link, function (e) {
+        e.preventDefault();
+
+        if (options.before) {
+            options.before(this);    
+        }
+
+        var classOpened = options.openClass || "open";
+        var parent = $(this).parents(options.parent);
+        var body = parent.find(options.hiddenContainer);
+        var opened = parent.hasClass(classOpened);
+
+        var v_up = 0;  
+        var v_down = 0;  
+        var bd = jQuery('body').get(0);
+
+        if (opened) {
+            $(body).slideUp({ 
+                easing: "linear", 
+                step: function(now, tween) {
+                    if (options.stepCorrection && tween.prop == "height"){
+                        if (v_up == 0){
+                            v_up = now;
+                        } else {
+                            var k = v_up - now;
+                            console.log("slideUp k", k);
+                            bd.scrollTop -= k;
+                            v_up = now;
+                        }
+                    }
+                }, 
+                duration: options.speed, 
+                complete: function() { 
+                    parent.removeClass(classOpened);
+
+                    if (options.complete) {
+                        options.complete("close");    
+                    }
+                }
+            });
+
+        } else {
+            $(body).slideDown({ 
+                easing: "linear", 
+                step: function(now, tween) {
+                    if (options.stepCorrection && tween.prop == "height"){
+                        if (v_down == 0){
+                            v_down = now;
+                        }else{
+                            var k = v_down - now;
+                            console.log("slideDown k", k);
+                            bd.scrollTop -= k;
+                            v_down = now;
+                        }
+                    }
+                }, 
+                duration: options.speed, 
+                complete: function() { 
+                    parent.addClass(classOpened);
+
+                    if (options.complete) {
+                        options.complete("open");    
+                    }
+                }
+            });
+        }
+    });  
+};
+
+var doit;
+$(window).resize(function(){
+    clearTimeout(doit);
+    doit = setTimeout(resizedw, 100);
+});
+
+var resizedw = function(){
+    // $('body').removeClass('show-slide-menu');
+    // $('body').removeClass('show-phones-menu');
+};
+
+var setFixedHeader = function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll > 0) {
+        $('header').addClass('header-scrolled');
+    } else {
+        $('header').removeClass('header-scrolled');
+    }
+}
